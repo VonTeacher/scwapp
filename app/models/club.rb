4768aaa461c80f55dcc39386e12089696b66f0c2
Vaughn Weiss <vaughn.weiss@gmail.com>
2017-06-18 08:02:54 +0000

    Add new club form

--- app/models/club.rb
+++ app/models/club.rb
@@ -6,20 +6,25 @@ class Club < ApplicationRecord
   validates :city, presence: true, allow_blank: false
   validates :state, presence: true, allow_blank: false, length: { is: 2 }
 
+  before_save   :downcase_then_titleize
+  before_create :downcase_then_titleize
+
   def self.search(search)
     if search
       where('lower(name) LIKE ?', "%#{search.downcase}%")
     else
-      all
+      all.order('name ASC')
     end
-  end
-
-  def self.last_five
-    all.order('created_at DESC').limit(5)
   end
 
   def date_added
     self.created_at.strftime("%B %d, %Y")
+  end
+
+  def downcase_then_titleize
+    self.name = self.name.downcase.titleize
+    self.city = self.city.downcase.titleize
+    self.state = self.state.upcase
   end
 
 end
