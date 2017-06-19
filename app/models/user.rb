a0b54c23c72cc3b8f9665f7a619ac180261a6895
Vaughn Weiss <vaughn.weiss@gmail.com>
2017-06-19 01:27:44 +0000

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   include Dateadded
 
+  before_save { self.email = email.downcase }
+
   validates :username, presence: true, length: { minimum: 6 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   def self.user_search(search)
     if search
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,54 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  
+  describe 'user model validations' do
+
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it 'should be valid with username and email' do
+      expect(@user).to be_valid
+    end
+
+    it 'should not be valid without a username' do
+      @user.username = ' '
+      expect(@user).not_to be_valid
+    end
+
+    it 'should require a username at least 6 characters long' do
+      @user.username = 'short'
+      expect(@user).not_to be_valid
+    end
+
+    it 'should not be valid without an email address' do
+      @user.email = ' '
+      expect(@user).not_to be_valid
+    end
+
+    it 'should not be valid with an incorrect email address' do
+      invalid_addresses = %w(foo@example,com foo_at_example.com foo@ex_ample.com foo@ex+ample.com foo@example.com.)
+      invalid_addresses.each do |i|
+        @user.email = i
+        expect(@user).not_to be_valid
+      end
+    end
+
+    it 'should require unique email addresses' do
+      duplicate_user = @user.dup
+      duplicate_user.email = @user.email.upcase
+      @user.save
+      expect(duplicate_user).not_to be_valid
+    end
+
+    it 'should save email addresses as lowercase' do
+      mixed_case_email = 'TesT@eXaMPLe.CoM'
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload
+      expect(mixed_case_email.downcase).to eq(@user.email)
+    end
+
+  end
 
 end
