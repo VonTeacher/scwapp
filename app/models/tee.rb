1f83d7bc27076f4a323fae89f3428628c083f172
Vaughn Weiss <vaughn.weiss@gmail.com>
2017-07-01 22:35:04 +0000

    Remove secondary tee validations

--- app/models/tee.rb
+++ app/models/tee.rb
@@ -8,11 +8,6 @@ class Tee < ApplicationRecord
   validates :color, presence: true
   validates :usga_course_rating_18, presence: true, numericality: true
   validates :slope_rating_18, presence: true, numericality: { only_integer: true }
-  validates :front_9_rating, numericality: true
-  validates :front_9_slope, numericality: { only_integer: true }
-  validates :back_9_rating, numericality: true
-  validates :back_9_slope, numericality: { only_integer: true }
-  validates :bogey_rating, numericality: true
   validates :gender, presence: true, length: { is: 1 }, inclusion: { in: %w(F M) }
   validates :club_id, presence: true
 
