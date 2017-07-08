1630cdd1dc0406a6f3a4f2f554166d7295c4bab5
Vaughn Weiss <vaughn.weiss@gmail.com>
2017-07-08 22:15:04 +0000

    Add custom color sort to tee listing on clubs page

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -3,7 +3,9 @@ module ClubsHelper
   def join_tees_with_commas club
     tee_array = []
     club.tees.each { |t| tee_array << t.color }
-    tee_array.join(', ')
+    tee_array.sort_by do |e|
+      ['blue', 'white', 'gold', 'green', 'red'].index(e)
+    end
   end
 
 end
