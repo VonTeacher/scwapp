d31148cbff9a2572c8fdcbac192ff7e4888ac12b
Vaughn Weiss <vaughn.weiss@gmail.com>
2017-07-08 22:25:09 +0000

    Club tee listing sort fix

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -2,7 +2,7 @@ module ClubsHelper
 
   def join_tees_with_commas club
     tee_array = []
-    color_order = ['blue', 'white', 'gold', 'green', 'red']
+    color_order = ['Blue', 'White', 'Gold', 'Green', 'Red']
     club.tees.each { |t| tee_array << t.color }
     sorted = tee_array.sort_by do |e|
       color_order.index(e)
