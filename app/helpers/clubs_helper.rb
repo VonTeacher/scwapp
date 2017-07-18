495cc74da27c26fe5da95c34f3bf9dc174cc78cd
Vaughn Weiss <vaughn.weiss@gmail.com>
2017-07-18 16:30:39 +0000

    Add custom tee color sort & various back-link buttons

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -10,4 +10,16 @@ module ClubsHelper
     sorted.join(', ')
   end
 
+  # THIS CODE NEEDS WORK!
+  # July 10, 2017
+  def list_tees_in_color_order club
+    tee_array = []
+    color_order = ['Blue', 'White', 'Gold', 'Green', 'Red']
+    club.tees.each { |t| tee_array << t }
+    sorted = tee_array.sort_by do |e|
+      color_order.index(e[:color])
+    end
+    sorted
+  end
+
 end
